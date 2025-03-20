75ec090fda418924bf4d30f6bc6a65d925d72a34
HemaTechno <ibrahimmansour4655@gmail.com>
2025-03-20 16:43:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const sidebar = document.getElementById("sidebar");
     sidebar.style.left = sidebar.style.left === "0px" ? "-250px" : "0px";
 }
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open");
+}
+
+function loadPage(page) {
+    document.getElementById("content-frame").src = page;
+}
